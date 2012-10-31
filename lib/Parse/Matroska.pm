d7272e160779d42b0413799f478377ee777b86d4
Kovensky <diogomfranco@gmail.com>
2012-10-31 17:47:21 +0000

    Write short documentation in Parse::Matroska

--- lib/Parse/Matroska.pm
+++ lib/Parse/Matroska.pm
@@ -1,8 +1,29 @@
 use strict;
 use warnings;
 
+# ABSTRACT: Module collection to parse Matroska files.
 package Parse::Matroska;
+=head1 NAME
 
-# ABSTRACT: parses Matroska files
+Parse::Matroska
+
+=head1 ABSTRACT
+
+Module collection to parse Matroska files.
+
+=head1 DESCRIPTION
+
+C<use>s L<Parse::Matroska::Reader>. See the documentation
+of the modules mentioned in L</"SEE ALSO"> for more information
+in how to use this module.
+
+=head1 SEE ALSO
+
+L<Parse::Matroska::Reader>, L<Parse::Matroska::Element>,
+L<Parse::Matroska::Definitions>.
+
+=cut
+
+use Parse::Matroska::Reader;
 
 1;
